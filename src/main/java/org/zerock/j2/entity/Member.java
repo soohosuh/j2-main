5d451e39fd1eed47e1d52fd0702db18a3fb97a62
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-07-17 18:10:25 +0900

    230717 FileUpload OneToMany process

---
+++ src/main/java/org/zerock/j2/entity/Member.java
@@ -0,0 +1,23 @@
+package org.zerock.j2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Member {
+
+    @Id
+    private String email;
+
+    private String pw;
+
+    private String nickName;
+
+    private boolean admin;
+}
